301f9a4f4dc23497236d93355c52e25566ac70f8
Alex Buck <buckatech@gmail.com>
2018-11-05 20:28:50 -0700

    added new tweet and styled

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,3 +4,20 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+function charCount() {
+  let cs = 140-$(this).val().length
+  if(cs < 0) {
+    $('#charLimit').css("color", "red");
+  } else {
+    $('#charLimit').css("color", "black");
+  }
+  $('#charLimit').text(cs)
+}
+
+$(document).ready(function() {
+  $('#subButton').on('click', function() {
+    console.log('fired')
+  })
+  $('#textAreaInput').keyup(charCount)
+  $('#textAreaInput').keydown(charCount)
+})
