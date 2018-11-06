cff4936aa17b36ec1331156b814cc696d95696e6
Alex Buck <buckatech@gmail.com>
2018-11-06 14:04:12 -0700

    add tweet element function done

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,6 +4,7 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+// Change .css to change the class instead of targeting css directly
 function charCount() {
   let cs = 140-$(this).val().length
   if(cs < 0) {
@@ -13,7 +14,58 @@ function charCount() {
   }
   $('#charLimit').text(cs)
 }
-
+data={
+  "user": {
+    "name": "Newton",
+    "avatars": {
+      "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+      "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+      "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+    },
+    "handle": "@SirIsaac"
+  },
+  "content": {
+    "text": "If I have seen further it is by standing on the shoulders of giants"
+  },
+  "created_at": 1461116232227
+}
+console.log(data)
+function createTweetElement() {
+  let html = 
+`<div class="row BodyRow1">
+<div class="col-3"></div>
+<div class="col-6">
+  <div class="row subrow">
+    <div class="col-1"></div>
+    <div class="col-10 opacParent">
+      <div class="row tweetContentHead">
+        <div class="col-2"><img class="bodyImg" src="/images/bird.png" alt=""></div>
+        <div class="col-8">
+          <h3 class="tweetH3">${data.user.name}</h3>
+        </div>
+        <div class="col-2"><span class="miniEmail">${data.user.handle}</span></div>
+      </div>
+      <div class="row tweetContentBody">
+        <div class="col-12">
+          <p class="tweetContent">${data.content.text}</p>
+        </div>
+      </div>
+      <div class="row tweetContentFoot">
+        <div class="col-custom-1"></div>
+        <div class="col-custom-22">
+          <p class="dateStamp">${data.created_at}</p>
+        </div>
+        <div class="col-custom-1"></div>
+      </div>
+    </div>
+    <div class="col-1"></div>
+  </div>
+</div>
+<div class="col-3"></div>
+</div>`
+  console.log(html)
+  }
+  createTweetElement()
 $(document).ready(function() {
   $('#subButton').on('click', function() {
     console.log('fired')
